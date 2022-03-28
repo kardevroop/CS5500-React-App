2e1d5395c44be4c736b28eae5d9288034d0f1f57
Devroop Kar <kardevroop@gmail.com>
2022-03-28 18:26:30 -0400

    Added dislike button and service

--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -5,7 +5,7 @@ import React from "react";
     super(props);
   }
   */
-  const TuitStats = ({tuit, likeTuit}) => {
+  const TuitStats = ({tuit, likeTuit, dislikeTuit}) => {
   //render() {
     return (
       <div className="row mt-2">
@@ -28,16 +28,30 @@ import React from "react";
           <span onClick={() => likeTuit(tuit)}>
             {
               tuit.stats.likes > 0 &&
-              <i className="fas fa-heart"
+              <i className="fa-solid fa-thumbs-up"
                 style={{color: 'red'}}></i>
             }
             {
               tuit.stats.likes <= 0 &&
-              <i className="far fa-heart"></i>
+              <i className="fa-light fa-thumbs-up"></i>
             }
             {tuit.stats && tuit.stats.likes}
           </span>
-      </div>
+        </div>
+        <div className="col">
+          <span onClick={() => dislikeTuit(tuit)}>
+            {
+              tuit.stats.dislikes > 0 &&
+              <i className="fa-solid fa-thumbs-down"
+                style={{color: 'red'}}></i>
+            }
+            {
+              tuit.stats.dislikes <= 0 &&
+              <i className="fa-light fa-thumbs-down"></i>
+            }
+            {tuit.stats && tuit.stats.dislikes}
+          </span>
+        </div>
       </div>
     );
   }
